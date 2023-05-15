bee66c6c0b687e43b8bc00d7441256556765331c
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-16 04:03:55 +0800

    displaying the tv1's together with tv2's channel and volume level using TestTV.py

--- TestTV.py
+++ TestTV.py
@@ -1,6 +1,11 @@
 import TV
 
+#Displaying the tv1's channel and volume level
 tv1 = TV.TV(30, 3)
 print("tv1's channel is", int(tv1.channel), "and volume level is", int(tv1.volume) )
+#Displaying the tv2's channel and volume level
+tv2 = TV.TV(3, 2)
+print("tv2's channel is", int(tv2.channel), "and volume level is", int(tv2.volume) )
+
 
 
