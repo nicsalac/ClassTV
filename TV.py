85f7524679dc71ca5c2a8dd58bc8c2dcc9e0e9cd
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-16 03:16:13 +0800

    first try printing tv1 channel and volume

--- TV.py
+++ TV.py
@@ -3,3 +3,18 @@ class TV:
     def __init__(self, channel, volume):
         self.channel = channel
         self.volume = volume
+    #Methods
+    def get_channel(self):
+        return self.channel
+
+    def get_volume(self):
+        return self.volume
+    
+    def set_channel(self, channel):
+        self.channel = channel
+
+    def set_volume(self, volume):
+        self.volume = volume
+
+tv1 = TV(30, 3)
+print("tv1's channel is", int(tv1.channel), "and volume level is", int(tv1.volume) )
