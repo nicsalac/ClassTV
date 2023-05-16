ddbc30ec1a3306fb421f871e023986db8f0d87bf
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-16 13:28:01 +0800

    add the on/off power as one of methods of a Class TV

--- TestTV.py
+++ TestTV.py
@@ -1,10 +1,10 @@
 import TV
 
 #Displaying the tv1's channel and volume level
-tv1 = TV.TV(30, 3)
+tv1 = TV.TV(30, 3, True)
 print("tv1's channel is", int(tv1.channel), "and volume level is", int(tv1.volume) )
 #Displaying the tv2's channel and volume level
-tv2 = TV.TV(3, 2)
+tv2 = TV.TV(3, 2, True)
 print("tv2's channel is", int(tv2.channel), "and volume level is", int(tv2.volume) )
 
 
