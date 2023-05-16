1a5ebe6b90a6fef160b3b07d1fa89201350442d9
nicsalac <nicoletteirishsalac127@gmail.com>
2023-05-16 12:21:17 +0800

    add on/off methods to class TV

--- TV.py
+++ TV.py
@@ -1,9 +1,18 @@
 class TV:
     #Construct a TV object
-    def __init__(self, channel, volume):
+    def __init__(self, channel, volume, on):
         self.channel = channel
         self.volume = volume
+        self.on = on
+        on = False
     #Methods
+
+    def power_off(self):
+        if self.on == True:
+            self.on += False
+        if self.on == False:
+            self.on += True
+
     def get_channel(self):
         return self.channel
 
@@ -16,7 +25,7 @@ class TV:
     def set_volume(self, volume):
         self.volume = volume
 
-tv1 = TV(30, 3)
-tv2 = TV(3, 2)
+tv1 = TV(30, 3, True)
+tv2 = TV(3, 2, True)
 print("tv1's channel is", int(tv1.channel), "and volume level is", int(tv1.volume) )
 print("tv2's channel is", int(tv2.channel), "and volume level is", int(tv2.volume) )
